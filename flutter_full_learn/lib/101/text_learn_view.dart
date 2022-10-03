9c732162d9d7537ef6e30f0965676bcd7052f175
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-03 23:51:59 +0300

    next to learning

--- flutter_full_learn/lib/101/text_learn_view.dart
+++ flutter_full_learn/lib/101/text_learn_view.dart
@@ -8,8 +8,10 @@ class TextLernView extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Text(
-          ('Buy the best one' * 10),
+          ('Buy the best one' * 20),
           maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.right,
           style: const TextStyle(
               wordSpacing: 2,
               letterSpacing: 3,
