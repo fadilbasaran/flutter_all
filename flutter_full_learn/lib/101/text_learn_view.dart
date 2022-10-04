54adf6397189ae03968642ee74f8ebfa07dd295f
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-04 15:55:36 +0300

    adding is class

--- flutter_full_learn/lib/101/text_learn_view.dart
+++ flutter_full_learn/lib/101/text_learn_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class TextLernView extends StatelessWidget {
-  const TextLernView({Key? key}) : super(key: key);
+  const TextLernView({Key? key, this.userName}) : super(key: key);
   final String name = 'Fadıl';
+  final String? userName;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,22 +15,57 @@ class TextLernView extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Text(
-          ('Welcome $name'),
-          maxLines: 2,
-          overflow:
-              TextOverflow.ellipsis, //Bu yapılar enum yapıları ile yapılıypr.
-          textAlign: TextAlign.right,
-          style: const TextStyle(
-              wordSpacing: 2,
-              decoration: TextDecoration.underline,
-              fontStyle: FontStyle.italic,
-              letterSpacing: 3,
-              color: Colors.orange,
-              fontSize: 16,
-              fontWeight: FontWeight.w600),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              ('Welcome $name ${name.length}'),
+              maxLines: 2,
+              overflow: TextOverflow
+                  .ellipsis, //Bu yapılar enum yapıları ile yapılıypr.
+              textAlign: TextAlign.right,
+              style: const TextStyle(
+                  wordSpacing: 2,
+                  decoration: TextDecoration.underline,
+                  fontStyle: FontStyle.italic,
+                  letterSpacing: 3,
+                  color: Colors.orange,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600),
+            ),
+            Text(
+              ('Hello $name ${name.length}'),
+              maxLines: 2,
+              overflow: TextOverflow
+                  .ellipsis, //Bu yapılar enum yapıları ile yapılıypr.
+              textAlign: TextAlign.right,
+              style: ProjectStyle.welcomeStyle,
+            ),
+            Text(
+              ('Hello $name ${name.length}'),
+              maxLines: 2,
+              overflow: TextOverflow
+                  .ellipsis, //Bu yapılar enum yapıları ile yapılıypr.
+              textAlign: TextAlign.right,
+              style: Theme.of(context).textTheme.headline5?.copyWith(
+                    color: Colors.yellow,
+                  ),
+            ),
+            Text(userName??''),
+          ],
         ),
       ),
     );
   }
+}
+
+class ProjectStyle {
+  static TextStyle welcomeStyle = const TextStyle(
+      wordSpacing: 2,
+      decoration: TextDecoration.underline,
+      fontStyle: FontStyle.italic,
+      letterSpacing: 3,
+      color: Colors.orange,
+      fontSize: 16,
+      fontWeight: FontWeight.w600);
 }
