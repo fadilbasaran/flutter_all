edfc14f3f026743fd3b615baf9056dc4b6969d34
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-05 17:08:14 +0300

    testing

--- flutter_full_learn/lib/101/color_learn.dart
+++ flutter_full_learn/lib/101/color_learn.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+ class ColorLearn extends StatelessWidget {
+   const ColorLearn({Key? key}) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Container();
+   }
+ }
