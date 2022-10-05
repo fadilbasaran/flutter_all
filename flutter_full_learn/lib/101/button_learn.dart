3ad09c618df70f9775dfdfdb0a3c9a72a7988eed
Cevdet Eren Özbozkurt <cevdet.eren.2001@gmail.com>
2022-10-05 14:44:14 +0300

    button learning devam

--- flutter_full_learn/lib/101/button_learn.dart
+++ flutter_full_learn/lib/101/button_learn.dart
@@ -8,9 +8,24 @@ class ButtonLearn extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
-        children:  [
-
-          TextButton(onPressed:(){} , child: const  Text('a'))
+        children: [
+          TextButton(onPressed: () {}, child: const Text('Save')),
+          ElevatedButton(onPressed: () {}, child: const Text('data')),
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.heart_broken_outlined)),
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.amber,
+                padding: const EdgeInsets.all(10),
+                shape: const CircleBorder()),
+            child: const Text('data'),
+          ),
+          InkWell(onTap: () {}, child: const Text('custom')),
         ],
       ),
     );
