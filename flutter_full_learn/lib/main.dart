59e5840d1cb2d0784598deef19d322eecfa0c3af
Cevdet Eren Özbozkurt <cevdet.eren.2001@gmail.com>
2022-10-05 11:44:00 +0300

    Scaffold is finished

--- flutter_full_learn/lib/main.dart
+++ flutter_full_learn/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_full_learn/101/container_sized_box_learn.dart';
+import 'package:flutter_full_learn/101/scafold_learn.dart';
 // ignore: unused_import
 import 'package:flutter_full_learn/101/text_learn_view.dart';
 
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home:  const ContainerSizedBoxLearn(),
+      home: const ScaffoldLearnView(),
     );
   }
 }
