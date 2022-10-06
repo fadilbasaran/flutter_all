a358dbe5014f9f8b3d53f8cb943d6afd88428a47
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-06 10:52:05 +0300

    padding is finished

--- flutter_full_learn/lib/main.dart
+++ flutter_full_learn/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_full_learn/101/color_learn.dart';
-import 'package:flutter_full_learn/101/stateless_learn.dart';
+import 'package:flutter_full_learn/101/padding_learn.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Learning',
       theme: ThemeData.dark().copyWith(
           errorColor: ColorsItems.sulu,
           appBarTheme: const AppBarTheme(
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
               systemOverlayStyle: SystemUiOverlayStyle.light,
               backgroundColor: Colors.transparent,
               elevation: 0)),
-      home: const StatelessLearn(),
+      home: const PaddinLearn(),
     );
   }
 }
