638d424a8559853eff65c1b20c45cfdaae38f72a
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-06 09:52:02 +0300

    Stateless is starting

---
+++ flutter_full_learn/lib/101/stateless_learn.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StatelessLearn extends StatelessWidget {
+  const StatelessLearn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- flutter_full_learn/lib/main.dart
+++ flutter_full_learn/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_full_learn/101/color_learn.dart';
+import 'package:flutter_full_learn/101/stateless_learn.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
               systemOverlayStyle: SystemUiOverlayStyle.light,
               backgroundColor: Colors.transparent,
               elevation: 0)),
-      home: const ColorLearn(),
+      home: const StatelessLearn(),
     );
   }
 }
