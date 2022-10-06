4068fdfd6e030f5ead692e514d1a23a0ff76d755
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-06 10:24:06 +0300

    Stateless is finished

--- flutter_full_learn/lib/101/stateless_learn.dart
+++ flutter_full_learn/lib/101/stateless_learn.dart
@@ -5,6 +5,48 @@ class StatelessLearn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children:  [
+          const TitleTextWidget(title: 'ali'),
+          const TitleTextWidget(title: 'ali1'),
+          const TitleTextWidget(title: 'ali2'),
+          const TitleTextWidget(title: 'ali3'),
+          const _CostumContainer(heigthCont: 100),
+          _emptyBox(),
+        ],
+      ),
+    );
+  }
+
+  SizedBox _emptyBox() => const SizedBox(height: 10);
+}
+
+class _CostumContainer extends StatelessWidget {
+  const _CostumContainer({Key? key, required this.heigthCont})
+      : super(key: key);
+  final double heigthCont;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: heigthCont,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.amber,
+      ),
+    );
+  }
+}
+
+class TitleTextWidget extends StatelessWidget {
+  const TitleTextWidget({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: Theme.of(context).textTheme.headline4,
+    );
   }
 }
