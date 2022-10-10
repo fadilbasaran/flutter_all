ddc3dbf37b948589115826e813edb94da12eff80
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-10 11:01:30 +0300

    Product, core and list tile are learned

---
+++ lib/101/list_tile_learn.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_full_learn/core/random_image.dart';
+
+class ListTileLearn extends StatelessWidget {
+  const ListTileLearn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  title: const RandomImage(),
+                  onTap: () {},
+                  subtitle: const Text('How do you use your card'),
+                  minVerticalPadding: 0,
+                  dense: true,
+                  leading: Container(
+                    height: 200,
+                    width: 30,
+                    alignment: Alignment.topCenter,
+                    color: Colors.red,
+                    child: const Icon(Icons.money),
+                  ),
+                  trailing: const SizedBox(
+                    width: 20,
+                    child: Icon(Icons.chevron_right),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_full_learn/101/color_learn.dart';
 import 'package:flutter_full_learn/101/indicator_learn.dart';
+import 'package:flutter_full_learn/101/list_tile_learn.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Learning',
       theme: ThemeData.dark().copyWith(
+      
         progressIndicatorTheme:const  ProgressIndicatorThemeData(
           color: Colors.white
         ),
@@ -28,7 +30,7 @@ class MyApp extends StatelessWidget {
               systemOverlayStyle: SystemUiOverlayStyle.light,
               backgroundColor: Colors.transparent,
               elevation: 0)),
-      home: const IndicatorLearn(),
+      home: const ListTileLearn(),
     );
   }
 }
