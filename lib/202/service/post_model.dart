413a8b6c80cf472cedc6610f7882b613954c7d5a
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-14 14:19:58 +0300

    Services is leanrning

--- lib/202/service/post_model.dart
+++ lib/202/service/post_model.dart
@@ -1,11 +1,11 @@
-class PostModel {
+class PostModelService {
   String? title;
   String? body;
   int? userId;
 
-  PostModel({this.title, this.body, this.userId});
+  PostModelService({this.title, this.body, this.userId});
 
-  PostModel.fromJson(Map<String, dynamic> json) {
+  PostModelService.fromJson(Map<String, dynamic> json) {
     title = json['title'];
     body = json['body'];
     userId = json['userId'];
--- lib/202/service/service_view_learn.dart
+++ lib/202/service/service_view_learn.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_full_learn/202/service/post_model.dart';
 
 class ServiceViewLearn extends StatefulWidget {
   const ServiceViewLearn({super.key});
@@ -8,15 +12,93 @@ class ServiceViewLearn extends StatefulWidget {
 }
 
 class _ServiceViewLearnState extends State<ServiceViewLearn> {
+  List<PostModelService>? _items;
+  String? name;
+  bool _isLoading = false;
+  final _basUrl = 'https://jsonplaceholder.typicode.com/';
+  late final Dio _networkManager;
+  @override
+  void initState() {
+    super.initState();
+    name = 'fadıl';
+    _networkManager = Dio(BaseOptions(baseUrl: _basUrl));
+    fetchPostItems();
+  }
+
+  void _changeLoading() {
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+  }
+
+  Future<void> fetchPostItems() async {
+    _changeLoading();
+    final response = await _networkManager.get('posts');
+    if (response.statusCode == HttpStatus.ok) {
+      //İşlem başarılı mı
+      final datas = response.data;
+      if (datas is List) {
+        setState(() {
+          _items = datas.map((e) => PostModelService.fromJson(e)).toList();
+        });
+      }
+    }
+    _changeLoading();
+  }
+
+  Future<void> fetchPostItemsAdvance() async {
+    _changeLoading();
+    final response = await _networkManager.get('posts');
+
+    if (response.statusCode == HttpStatus.ok) {
+      //İşlem başarılı mı
+      final datas = response.data;
+      if (datas is List) {
+        setState(() {
+          _items = datas.map((e) => PostModelService.fromJson(e)).toList();
+        });
+      }
+    }
+    _changeLoading();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          _isLoading
+              ? const CircularProgressIndicator.adaptive()
+              : const SizedBox.shrink()
+        ],
+        title: Text(name ?? ' '),
+      ),
       body: ListView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        itemCount: _items?.length ?? 0,
         itemBuilder: (BuildContext context, int index) {
-          return const Text('data');
+          return _PostCard(model: _items?[index]);
         },
       ),
+    );
+  }
+}
+
+class _PostCard extends StatelessWidget {
+  const _PostCard({
+    Key? key,
+    required PostModelService? model,
+  })  : _model = model,
+        super(key: key);
+
+  final PostModelService? _model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: ListTile(
+          title: Text(_model?.title ?? ''), subtitle: Text(_model?.body ?? '')),
     );
   }
 }
