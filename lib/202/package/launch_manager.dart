0043347c0ad8073408840bf2143a512a2e458fe6
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-15 11:23:10 +0300

    Lunchurl is learned

--- lib/202/package/launch_manager.dart
+++ lib/202/package/launch_manager.dart
@@ -1,6 +1,6 @@
 import 'package:url_launcher/url_launcher_string.dart';
 
-mixin LunchMixin{//Bunu yanına 
+mixin LunchMixin{//Bunu yanına ör: 'on StatlessWidget' yazınca sadece istediğimiz Widget vey sınıf türetebilir
 //sadece bir amaca yönlik yapılan işlemler
 //Constructor oluşturulamıyor
 void launchUrl(String url) async {
