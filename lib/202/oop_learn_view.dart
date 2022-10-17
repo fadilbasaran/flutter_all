34003b0ead4f295374528131200e5d77788048b9
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-17 08:51:29 +0300

    Last win

---
+++ lib/202/oop_learn_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OOPLeanrView extends StatefulWidget {
+  const OOPLeanrView({super.key});
+
+  @override
+  State<OOPLeanrView> createState() => _OOPLeanrViewState();
+}
+
+class _OOPLeanrViewState extends State<OOPLeanrView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      
+    );
+  }
+}
