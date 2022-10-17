0149534066ef37189e28034d988221ec85cbc732
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-17 18:00:04 +0300

    iMac latest version

--- lib/202/cache/user_cache/user_cache_manager.dart
+++ lib/202/cache/user_cache/user_cache_manager.dart
@@ -1,16 +1,28 @@
+import 'dart:convert';
+
 import 'package:flutter_full_learn/202/cache/shared_learn_cache_view.dart';
 import 'package:flutter_full_learn/202/cache/shared_manager.dart';
 
 class UserCaheManger {
   final SharedManager sharedManager;
 
-  final List<User> items;
+  UserCaheManger(this.sharedManager);
+  void saveItems(List<User> items) {
+    //jsona dönüştür
+//compute
+    final _item = items.map((element) => jsonEncode(element)).toList();
 
-  UserCaheManger(this.sharedManager, this.items);
-  void saveItems() {
+    sharedManager.saveItmesString(SharedKeys.users, _item);
+  }
 
-    
+  List<User>? getItems() {
+    final itemString = sharedManager.getItemsString(SharedKeys.users);
 
-sharedManager.saveItmesString(SharedKeys.users,)
+    if (itemString?.isNotEmpty ?? false) {
+      return itemString!.map((e) {
+        return User('name', 'descriptoin', 'url');
+      }).toList();
+    }
+    return null;
   }
 }
