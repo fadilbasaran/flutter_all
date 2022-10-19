040cf31d639a41b6aa26a2dbeb644cc5233b69f4
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-19 14:46:23 +0300

    Geeric is done

--- lib/303/generic_learn.dart
+++ lib/303/generic_learn.dart
@@ -1,8 +1,20 @@
-// ignore_for_file: avoid_print
+// ignore_for_file: avoid_print, unused_local_variable, no_leading_underscores_for_local_identifiers
 
-class UserManagement {
+class UserManagement<T extends AdminUser> {
+  final T admin;
+
+  UserManagement(this.admin);
   void sayName(GenericUser user) {
     print(user.name);
+  }
+
+  Iterable<FBModel<R>>? showNames<R>(List<GenericUser> users) {
+    if (R == String) {
+      final names =
+          users.map((e) => FBModel<R>(e.name.split('').toList().cast<R>()));
+      return names;
+    }
+    return null;
   }
 
   int calculateMoney(List<GenericUser> users) {
@@ -10,12 +22,14 @@ class UserManagement {
     for (var item in users) {
       sum += item.money;
     }
+    //Eğer admin gelirse 15 eklenilsin
+    int initialValue = admin.role == 1 ? admin.money : 0;
 
-    final sumMoney = users
-        .map((e) => e.money)
-        .fold(0, (previousValue, element) => previousValue + element);
-    return users.fold(
-        0, (previousValue, element) => previousValue + element.money);
+    final sumMoney = users.map((e) => e.money).fold<int>(
+        initialValue, (previousValue, element) => previousValue + element);
+    final _money = users.fold<int>(initialValue,
+        (previousValue, element) => previousValue + element.money);
+    return _money;
   }
 }
 
@@ -26,4 +40,16 @@ class GenericUser {
   final int money;
 
   GenericUser(this.name, this.id, this.money);
+}
+
+class AdminUser extends GenericUser {
+  final int role;
+  AdminUser(super.name, super.id, super.money, this.role);
+}
+
+class FBModel<T> {
+  final String name = 'vb';
+  final List<T> items;
+
+  FBModel(this.items);
 }
---
+++ test/user_manager_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_full_learn/303/generic_learn.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  setUp(() {});
+
+  test(
+    'Calculate testing',
+    () {
+      final users = [
+        GenericUser('Fb', '23', 30),
+        GenericUser('Fb', '23', 30),
+        GenericUser('Fb', '23', 30),
+      ];
+      final userManagment = UserManagement(AdminUser('vadmin', '1', 15, 2));
+      final result = userManagment.calculateMoney(users);
+      final response = userManagment.showNames<String>(users);
+      expect(result, 90);
+    },
+  );
+}
