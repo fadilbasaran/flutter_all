56dcb210352ddbc36ee58bd95da646960be5d0ae
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-21 18:01:08 +0300

    46:52 last macos

--- test/list_test.dart
+++ test/list_test.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter_full_learn/303/generic_learn.dart';
+import 'package:flutter_full_learn/product/widget/card/high_card.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -10,7 +13,8 @@ void main() {
       GenericUser('fb', '1', 34),
       GenericUser('fb', '1', 34),
     ];
-
-    
+    List<HighCard> higCards = users.map((e) {
+      return HighCard(items: e.name.split('').toList());
+    }).toList();
   });
 }
