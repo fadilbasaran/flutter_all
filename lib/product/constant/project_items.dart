9f48d6934f713d5b88f5765da0a9879ddb821567
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-21 17:30:02 +0300

    Singleton learned

---
+++ lib/product/constant/project_items.dart
@@ -0,0 +1,7 @@
+
+class ProjectItems {
+  ProjectItems._(); //hiç bir nesne üretilemiyor.
+
+  static const String projectName = 'FB94';
+}
+
---
+++ test/list_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_full_learn/303/generic_learn.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  setUp(() {});
+
+  test('List Best of', () {
+    final users = [
+      GenericUser('fb', '1', 34),
+      GenericUser('fb', '1', 34),
+      GenericUser('fb', '1', 34),
+    ];
+
+    
+  });
+}
