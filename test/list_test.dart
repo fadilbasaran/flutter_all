533e7680d76aef73792c7504112c04d13ac58f3f
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-22 12:34:24 +0300

    Extansion is added

--- test/list_test.dart
+++ test/list_test.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unused_local_variable
 
+import 'package:collection/collection.dart';
 import 'package:flutter_full_learn/303/generic_learn.dart';
 import 'package:flutter_full_learn/product/widget/card/high_card.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,14 +10,40 @@ void main() {
 
   test('List Best of', () {
     final users = [
-      GenericUser('fb', '1', 34),
-      GenericUser('fb', '1', 34),
-      GenericUser('fb', '1', 34),
+      GenericUser('fb1', '1', 34),
+      GenericUser('fb2', '2', 40),
+      GenericUser('fb3', '3', 50),
     ];
     List<HighCard> higCards = users.map((e) {
       return HighCard(items: e.name.split('').toList());
     }).toList();
 
-    users.singleWhere((element) => element.findUserName(element.name));
+    try {
+      final response = users.singleWhere(
+        (element) => element.findUserName('x'),
+        orElse: () {
+          return GenericUser('null', 'id', 15);
+        },
+      );
+      print(response.name);
+    } catch (e) {
+      prints(e);
+    }
+  });
+
+  test('List Best of with collaction', () {
+    final users = [
+      GenericUser('fb1', '1', 34),
+      GenericUser('fb2', '2', 40),
+      GenericUser('fb3', '3', 50),
+    ];
+    List<HighCard> higCards = users.map((e) {
+      return HighCard(items: e.name.split('').toList());
+    }).toList();
+
+    final response = users.singleWhereOrNull(
+      (element) => element.findUserName('x'),
+    );
+    prints(response);
   });
 }
