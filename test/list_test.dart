343c2ca04cb0766e284e6b6678439546dfb02cf2
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-10-24 23:59:34 +0300

    List learning

--- test/list_test.dart
+++ test/list_test.dart
@@ -8,43 +8,54 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   setUp(() {});
 
-  test('List Best of', () {
-    final users = [
-      const GenericUser('fb1', '1', 34),
-      const GenericUser('fb2', '2', 40),
-      const GenericUser('fb3', '3', 50),
-    ];
-    List<HighCard> higCards = users.map((e) {
-      return HighCard(items: e.name.split('').toList());
-    }).toList();
+  test(
+    'List Best of',
+    () {
+      final users = [
+        const GenericUser('fb1', '1', 34),
+        const GenericUser('fb2', '2', 40),
+        const GenericUser('fb3', '3', 50),
+      ];
+      List<HighCard> higCards = users.map((e) {
+        return HighCard(items: e.name.split('').toList());
+      }).toList();
 
-    try {
-      final response = users.singleWhere(
-        (element) => element.findUserName('x'),
-        orElse: () {
-          return const GenericUser('Ab valla yok', 'id', 15);
-        },
-      );
-      print(response.name);
-    } catch (e) {
-      prints(e);
-    }
+      try {
+        final response = users.singleWhere(
+          (element) => element.findUserName('x'),
+          orElse: () {
+            return const GenericUser('Ab valla yok', 'id', 15);
+          },
+        );
+        print(response.name);
+      } catch (e) {
+        prints(e);
+      }
 
-    print('${users.where((element) => element.money > 40)}');
-    users.addAll([const GenericUser('eklend', 'id', 15)]);
-    users.lastWhere((element) =>
-        element.id == '5'); //Listenin en sonundan bana idsi 5 olanı getir
+      print('${users.where((element) => element.money > 40)}');
+      users.addAll([const GenericUser('eklend', 'id', 15)]);
+      users.lastWhere((element) =>
+          element.id == '5'); //Listenin en sonundan bana idsi 5 olanı getir
 
-    users.take(5); //içinden 5 tane getir.
-    users.remove(const GenericUser('fb1', '1', 34)); //id göre siler
-    users.removeAt(2); //indexe göre siler
-    users.removeWhere((element) => element.id == '10'); //id on olanı sil
+      users.take(5); //içinden 5 tane getir.
+      users.remove(const GenericUser('fb1', '1', 34)); //id göre siler
+      users.removeAt(2); //indexe göre siler
+      users.removeWhere((element) => element.id == '10'); //id on olanı sil
 
-    final result = users.indexWhere((element) => element.id == '1');
-    if (result > 0) {
-      users[result] = const GenericUser('Ali', '332', 3434);
-    }
-  });
+      final result = users.indexWhere((element) => element.id == '1');
+      if (result >= 0) {
+        users[result] = const GenericUser('Ali', '332', 3434);
+      }
+
+      //users.map((e) => e.money).where((element) => element > 5);//Yan yan komutlar yazılabiliyor.
+
+      users.expand((element) => [const GenericUser('ali', '23', 12)]);
+      //insert etmek yerine büyütüyor Listeyi,
+      users.sample(3); //Rastegele kaç değer istiyorsan san geri veriyor
+
+      users.contains(const GenericUser('fb3', '3', 50));
+    },
+  );
 
   test('List Best of with collaction', () {
     final users = [
@@ -61,6 +72,12 @@ void main() {
     );
     users.lastWhereOrNull(
         (element) => element.id == '5'); //Varsa getir eğer yoksa null döndür
+
+    users.lastOrNull;
+
+    users.forEachIndexed((index,
+        element) {}); //Hem elementi hem de kaçıncı sırada olduğuna bakabiliyorsun.
+
     prints(response);
   });
 }
