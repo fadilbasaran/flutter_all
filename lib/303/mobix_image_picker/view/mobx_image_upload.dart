c9182c0284bb85419dc1a74d396ea086b2c212f3
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-11-19 22:23:46 +0300

    mobx used

--- lib/303/mobix_image_picker/view/mobx_image_upload.dart
+++ lib/303/mobix_image_picker/view/mobx_image_upload.dart
@@ -1,6 +1,11 @@
 // ignore_for_file: unused_field
 
 import 'package:flutter/material.dart';
+import 'package:flutter_full_learn/303/mobix_image_picker/view_model/image_upload_view_model.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:lottie/lottie.dart';
+
+import '../../../product/utility/image_upload.dart';
 
 class MobxImageUpload extends StatefulWidget {
   const MobxImageUpload({super.key});
@@ -12,25 +17,52 @@ class MobxImageUpload extends StatefulWidget {
 class _MobxImageUploadState extends State<MobxImageUpload> {
   final String _imageUploadLottiePath =
       'https://assets3.lottiefiles.com/packages/lf20_urbk83vw.json';
+
+  final _imageUploadViewModel = ImageUploadViewModel();
+  final _imageUploadManager = ImageUploadManager();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Image Upload'),
+        actions: [
+          Observer(builder: (_) {
+            return _imageUploadViewModel.isLoading
+                ? const FittedBox(
+                    child: CircularProgressIndicator(color: Colors.white))
+                : const SizedBox.shrink();
+          })
+        ],
       ),
       body: Column(
         children: [
-          Expanded(
-              flex: 2,
-              child: Card(
-                  elevation: 10,
-                  child: FittedBox(
-                    child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.upload_file_outlined)),
-                  ))),
+          Row(
+            children: [
+              Expanded(
+                  flex: 2,
+                  child: Card(
+                      elevation: 10,
+                      child: Row(
+                        children: [
+                          Observer(builder: (_) {
+                            return Expanded(
+                                flex: 4,
+                                child: _imageUploadViewModel.file != null
+                                    ? Image.file(_imageUploadViewModel.file!)
+                                    : const SizedBox());
+                          }),
+                          IconButton(
+                              onPressed: () {
+                                _imageUploadManager.fetchFromLibrary();
+                              },
+                              icon: FittedBox(
+                                  child:
+                                      Lottie.network(_imageUploadLottiePath))),
+                        ],
+                      ))),
+            ],
+          ),
           const Divider(),
-          const Spacer(flex: 4),
         ],
       ),
     );
--- lib/303/mobix_image_picker/view_model/image_upload_view_model.dart
+++ lib/303/mobix_image_picker/view_model/image_upload_view_model.dart
@@ -1,8 +1,28 @@
+// ignore_for_file: depend_on_referenced_packages, library_private_types_in_public_api
+
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 import 'package:mobx/mobx.dart';
 part 'image_upload_view_model.g.dart';
 
-class ImageUploadViewModel = _ImageUploadViewModelBase with _$ImageUploadViewModel;
+class ImageUploadViewModel = _ImageUploadViewModelBase
+    with _$ImageUploadViewModel;
 
 abstract class _ImageUploadViewModelBase with Store {
-  
+  @observable 
+  File? file;
+  @observable //Ekranda dinlemek istenliyorsa
+  bool isLoading = false;
+
+  @action //Ekran bir haber verilecek ise
+  void changeLoading() {
+    isLoading = !isLoading;
+  }
+   @action
+  void saveLocalFile(XFile? file) {
+    if (file == null) return;
+    this.file = File(file.path);
+  }
+
+  intit() {}
 }
