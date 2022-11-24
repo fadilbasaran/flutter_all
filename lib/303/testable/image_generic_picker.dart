0fc38c2edad99c964e774486dca4f618e702a251
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-11-24 22:59:16 +0300

    Image generic Upload Manager

---
+++ lib/303/testable/image_generic_picker.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ImageUploadManager extends StatefulWidget {
+  const ImageUploadManager({super.key});
+
+  @override
+  State<ImageUploadManager> createState() => _ImageUploadManagerState();
+}
+
+class _ImageUploadManagerState extends State<ImageUploadManager> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+body:Column(children: [],)
+    );
+  }
+}
